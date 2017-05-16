ddd2ed748f1c896d202d4a3571b8edb04505e640
João Avelino <joao.manuel.avelino@gmail.com>
2017-05-16 09:57:36 +0100

    initial commit

---
+++ SamplePureJavaProject/src/main/java/foo/Foo.java
@@ -0,0 +1,12 @@
+package foo;
+
+/**
+ * Created by João Avelino on 5/16/2017.
+ */
+
+public class Foo {
+    public static void main(String[] arg) {
+        String hi = "Hello World";
+        System.out.println(hi);
+    }
+}
